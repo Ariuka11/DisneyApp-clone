a89ab91d1fc9f6c9d95676b6660a9093ccb600c4
ariuka11 <ariun1118@gmail.com>
2022-01-16 12:49:28 -0500

    finished all the components

--- disney-clone-app/components/MoviesCollection.jsx
+++ disney-clone-app/components/MoviesCollection.jsx
@@ -1,4 +1,5 @@
 import MovieThumbnail from "./MovieThumbnail";
+
 function MoviesCollection({ results, title }) {
   return (
     <div className="'relative flex flex-col space-y-2 my-10 px-8 max-w-[1400px] mx-auto">
